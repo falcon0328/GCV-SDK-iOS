b75260853011764043333c16726c1730df097818
aseo <aseo@yahoo-corp.jp>
2019-01-27 11:06:52 +0900

    refactor: Carthage対応を行った

---
+++ Carthage/Build/iOS/GCV_SDK.framework/Headers/GCV_SDK.h
@@ -0,0 +1,19 @@
+//
+//  GCV_SDK.h
+//  GCV-SDK
+//
+//  Created by aseo on 2019/01/27.
+//  Copyright © 2019年 Falcon Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GCV_SDK.
+FOUNDATION_EXPORT double GCV_SDKVersionNumber;
+
+//! Project version string for GCV_SDK.
+FOUNDATION_EXPORT const unsigned char GCV_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GCV_SDK/PublicHeader.h>
+
+
